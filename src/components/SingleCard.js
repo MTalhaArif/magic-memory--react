b2374d7be2149a1c5ebe5393b5d32711f28ddc87
talhaarif <108628697+MTalhaArif@users.noreply.github.com>
2022-10-13 18:51:22 +0500

    project made

---
+++ src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import './SingleCard.css'
+const SingleCard = ({card, handleChoice, flipped , disabled}) => {
+//click handle
+    const handleClick=()=>{
+        if (!disabled){
+        handleChoice(card);
+        }
+    }
+
+    return ( 
+    <div className="card" >
+        <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} alt="card front" className="front" />   
+        <img src="./img/cover.png" 
+        alt="card back"
+         className="back"
+         onClick={handleClick} />
+        </div>
+
+</div> 
+     );
+}
+ 
+export default SingleCard;
